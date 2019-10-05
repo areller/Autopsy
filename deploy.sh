4a94a424134462b5412f3a4c2b20f0496c2446c4
areller <lekararik@gmail.com>
2019-10-05 14:10:31 +0300

    Changed to autopsy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dotnet pack -c Release -p:PackageVersion=$TRAVIS_TAG
-dotnet nuget push src/LiveDelegate.ILSpy/bin/Release/LiveDelegate.ILSpy.$TRAVIS_TAG.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
+dotnet nuget push src/Autopsy.ILSpy/bin/Release/Autopsy.ILSpy.$TRAVIS_TAG.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
